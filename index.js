d5f37fa4f2916af6b32b4516117cbc48ddeb981f
azu <azuciao@gmail.com>
2020-07-30 00:56:29 +0900

    fix

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
-const app = express()
+const app = express();
 const { compile } = require("@textlint/compiler");
 
 const wrap = fn => (req, res, next) => fn(req, res, next).catch(next);
