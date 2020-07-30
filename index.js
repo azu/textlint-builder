a86593485d7e7710fe81da2de50e641bdf2c6f6e
azu <azuciao@gmail.com>
2020-07-31 00:56:11 +0900

    fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 const path = require("path");
 const express = require("express");
 const app = express();
@@ -6,33 +6,41 @@ const { compile } = require("@textlint/compiler");
 
 const wrap = fn => (req, res, next) => fn(req, res, next).catch(next);
 
-app.get("/", wrap(async (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-}));
+app.get(
+    "/",
+    wrap(async (req, res) => {
+        res.sendFile(__dirname + "/index.html");
+    })
+);
 
-app.get("/textlint.js", wrap(async (req, res) => {
-    const outDir = path.join(__dirname, "dist");
-    const outFile = path.join(outDir, "textlint.js");
+app.get(
+    "/textlint.js",
+    wrap(async (req, res) => {
+        const outDir = path.join(__dirname, "dist");
+        const outFile = path.join(outDir, "textlint.js");
 
-    if (fs.existsSync(outFile)) {
-        res.set({ 'content-type': 'text/javascript; charset=utf-8' });
-        return res.sendFile(path.join(outDir, "textlint.js"))
-    }
-    await compile({
-        compileTarget: "webworker",
-        mode: "production",
-        configFilePath: path.join(__dirname, ".textlintrc.json"),
-        outputDir: outDir,
-    });
-    if (!fs.existsSync(outFile)) {
-        res.status(500)
-        res.render("Can not compile");
-        return;
-    }
-    res.set({ 'content-type': 'text/javascript; charset=utf-8' });
-    return res.sendFile(path.join(outDir, "textlint.js"));
-}));
+        if (fs.existsSync(outFile)) {
+            res.set({ "content-type": "text/javascript; charset=utf-8" });
+            const content = await fs.promises.readFile(outFile, "utf-8");
+            return res.send(content);
+        }
+        await compile({
+            compileTarget: "webworker",
+            mode: "production",
+            configFilePath: path.join(__dirname, ".textlintrc.json"),
+            outputDir: outDir
+        });
+        if (!fs.existsSync(outFile)) {
+            res.status(500);
+            res.render("Can not compile");
+            return;
+        }
+        res.set({ "content-type": "text/javascript; charset=utf-8" });
+        const content = await fs.promises.readFile(outFile, "utf-8");
+        return res.send(content);
+    })
+);
 
 app.listen(8080, () => {
     console.log("App listening on port 8080!");
-})
+});
